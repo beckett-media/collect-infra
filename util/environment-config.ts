418b1534685eb2b6a65e5d3084fc42e9d11b677a
Damian Winkowski <damian.winkowski@outlook.com>
2023-01-12 12:46:28 +0100

    populating env parameters

--- util/environment-config.ts
+++ util/environment-config.ts
@@ -37,29 +37,27 @@ const environmentConfig = (environmentName: string): IEnvironmentConfig => {
     };
   } = {
     dev: {
-      backup: true,
-      vpnServerCertificateArn:
-        "arn:aws:acm:us-east-1:223560911013:certificate/e0f2d3ef-1695-4cbd-bff1-76020d430a4b",
-      vpnClientCertificateArn:
-        "arn:aws:acm:us-east-1:223560911013:certificate/fe939073-ac74-409b-acb6-7d24b6f8d496",
-      domainName: "beckettcollectdev.com",
+      backup: false,
+      vpnServerCertificateArn: "",
+      vpnClientCertificateArn: "",
+      domainName: "collect-dev.beckett.com",
       rootAccountId: "750497448356",
-      collectApiHttpApiId: "e2hleqcy71",
-      ssoApiHttpApiId: "dautelb593",
-      isBeckett: false,
-      awsAccountId: "",
-      awsRegion: "",
-      vpcId: "",
-      publicSubnetsIds: [],
-      publicSubnetsRtbIds: [],
-      privateSubnetsIds: [],
-      privateSubnetsRtbIds: [],
+      collectApiHttpApiId: "",
+      ssoApiHttpApiId: "",
+      isBeckett: true,
+      awsAccountId: "438371105488",
+      awsRegion: "us-east-1",
+      vpcId: "vpc-0e9ba925154229a5a",
+      publicSubnetsIds: ["subnet-0999888708dc2ec6d", "subnet-02b88877a9592faa2"],
+      publicSubnetsRtbIds: ["rtb-01db24de97a30ca4c", "rtb-0f99400c8c8ceac64"],
+      privateSubnetsIds: ["subnet-02aa7000875bb691f", "subnet-01710af3304eea65b"],
+      privateSubnetsRtbIds: ["rtb-0ebfdeece6ab8d35c", "rtb-055c525d5c25731e6"],
     },
     staging: {
-      backup: false, // Whether or not the database and S3 buckets should be backed up with AWS Backup
+      backup: true, // Whether or not the database and S3 buckets should be backed up with AWS Backup
       vpnServerCertificateArn: "", // See instructions in the readme in the VPN section
       vpnClientCertificateArn: "", // See instructions in the readme in the VPN section
-      domainName: "beckettcollectstaging.com", // The domain you register prior to deployment. See "Collect Frontend" in the README
+      domainName: "collect-preprod.beckett.com", // The domain you register prior to deployment. See "Collect Frontend" in the README
       rootAccountId: "750497448356", // The root AWS account of which this account is part of
       collectApiHttpApiId: "", // The API Gateway id of the collect api, which is deployed seperately. Note: This CDK code must be deployed prior to the collect api service. After this is deployed, you can deploy the api, grab the id, put it here and redeploy
       ssoApiHttpApiId: "", // The API Gateway id of the sso api, which is deployed seperately. Note: This CDK code must be deployed prior to the sso api service. After this is deployed, you can deploy SSO, grab the id, put it here and redeploy
@@ -73,21 +71,21 @@ const environmentConfig = (environmentName: string): IEnvironmentConfig => {
       privateSubnetsRtbIds: ["rtb-0f0d2e4a52538fd47", "rtb-0193067819ab5d157"],
     },
     production: {
-      backup: false,
+      backup: true,
       vpnServerCertificateArn: "",
       vpnClientCertificateArn: "",
-      domainName: "beckettcollectproduction.com",
+      domainName: "collect.beckett.com",
       rootAccountId: "750497448356",
       collectApiHttpApiId: "",
       ssoApiHttpApiId: "",
       isBeckett: true,
-      awsAccountId: "",
-      awsRegion: "",
-      vpcId: "",
-      publicSubnetsIds: [],
-      publicSubnetsRtbIds: [],
-      privateSubnetsIds: [],
-      privateSubnetsRtbIds: [],
+      awsAccountId: "796687042250",
+      awsRegion: "us-east-1",
+      vpcId: "vpc-0bc8ded0b6104e9f4",
+      publicSubnetsIds: ["subnet-0caff20fea9ae9e82", "subnet-0d948079c31a3a56c"],
+      publicSubnetsRtbIds: ["rtb-02d9a54fd96d06378", "rtb-041f584a57c4f9a82"],
+      privateSubnetsIds: ["subnet-0109e5779cb86ddaa", "subnet-0b2b877a705c8ca01"],
+      privateSubnetsRtbIds: ["rtb-0d61784f4d7c9a379", "rtb-080e473d7a8ca4f64"],
     },
   };
   return environmentMapper[environmentName];
